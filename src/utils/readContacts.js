430fdc0f581046ba0ea5aef9d133bcd5fe795cee
Yurii Kit <insidelkit@gmail.com>
2025-06-29 11:49:49 +0300

    updata readContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -5,7 +5,7 @@ export const readContacts = async () => {
   try {
     const data = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
     const contacts = JSON.parse(data);
-    console.log('Розпарсені дані:', contacts);
+
     return contacts;
   } catch (error) {
     console.error('Помилка при читанні файлу:', error);
